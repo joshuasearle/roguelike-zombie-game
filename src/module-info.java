12e9fce53393cb5a4d63200178f10e2e6eb4fc97
joshuasearle <joshryansearle@gmail.com>
2020-08-14 22:54:17 +1000

    Remove module.info

--- src/module-info.java
+++
@@ -1,7 +0,0 @@
-module project {
-	exports edu.monash.fit2099.demo.mars;
-	exports game;
-	exports edu.monash.fit2099.interfaces;
-	exports edu.monash.fit2099.engine;
-	exports edu.monash.fit2099.demo.conwayslife;
-}
